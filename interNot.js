f06ea8e450e10288d9b161637876a16d2ba3738f
Vivek Bhookya <v.bhookya98@gmail.com>
2018-07-03 21:24:08 -0500

    InterNot!

--- interNot.js
+++ interNot.js
@@ -1,15 +1,7 @@
+// InterNot!
+// Stay off the web
+// @author Vivek Bhookya
 
-
-window.location.href=""
-
-// var htmlToAdd = '<!DOCTYPE html><html><body><p align="center">Go outside, read a book, learn to cook, sleep, call your friends, talk to your family...stay off the internet.</p><br><p align="center"><em>InterNot!</em></p></body></html>';
-
-// document.removeChild(document.documentElement);
-
-// window.location.href="chrome-extension://jejedjlleijljkmbbjhampffhlcddclc/interNot.html";
-
-//var parent = document.createElement('div');
-//parent.innerHTML = htmlToAdd;
-//document.parentNode.insertAfter(parent, document);
-
-//console.log('parent: + ' + parent.textContent);
+if (window.location.href !== "https://mrvivacious.github.io/InterNot/") {
+  window.location.href="https://mrvivacious.github.io/InterNot/";
+}
